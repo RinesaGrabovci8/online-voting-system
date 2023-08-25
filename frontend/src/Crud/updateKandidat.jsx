9eb3d174d61bdf0367e4d64a46d5a6c398f4d6b9
RinesaGrabovci8 <rinesagrabovci08@gmail.com>
2023-08-26 02:30:28 +0300

    .

---
+++ frontend/src/Crud/updateKandidat.jsx
@@ -0,0 +1,79 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+export default function UpdateKandidat() {
+  const [dataForm, setDataForm] = useState({
+    name: "",
+    surname: "",
+    party:"",
+    election:""
+  });
+
+  const handleButtonClick = () => {
+    console.log('Button Clicked');
+    axios.put("http://localhost:5000/crud/candidate", dataForm).then((res) => {
+      console.log('res', res);
+    })
+  };
+
+  console.log('dataForm', dataForm);
+
+  const changes = (e) => {
+    setDataForm({ ...dataForm, [e.target.name]: e.target.value });
+  }
+
+  return (
+    <Box border={1} p={3} maxWidth={400} style={{marginTop: 100, marginLeft: 650, marginBottom:50}}>
+      <Typography variant="h6" gutterBottom>
+        Perditeso Kandidate
+      </Typography>
+      <TextField
+        label="Emri"
+        variant="outlined"
+        fullWidth
+        name='name'
+        margin="normal"
+        value={dataForm.name}
+        onChange={(e) => changes(e)}
+      />
+      <TextField
+        label="Mbiemri"
+        variant="outlined"
+        fullWidth
+        name='surname'
+        margin="normal"
+        value={dataForm.surname}
+        onChange={(e) => changes(e)}
+      />
+      <TextField
+        label="Partia"
+        variant="outlined"
+        fullWidth
+        name='party'
+        margin="normal"
+        value={dataForm.party}
+        onChange={(e) => changes(e)}
+      />
+      <TextField
+        label="Zgjedhjet"
+        variant="outlined"
+        fullWidth
+        name='election'
+        margin="normal"
+        value={dataForm.election}
+        onChange={(e) => changes(e)}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleButtonClick}
+        style={{ marginTop: '16px' }}
+        >
+           Perditeso 
+        </Button>
+    </Box>
+  );
+}
